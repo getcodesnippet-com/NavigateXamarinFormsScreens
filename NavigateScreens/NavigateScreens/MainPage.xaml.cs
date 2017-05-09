93606540b82d29fe128f8e8e13bfa532c03b55c7
getcodesnippet-com <info@getcodesnippet.com>
2017-05-09 21:31:24 +0500

    initial commit

---
+++ NavigateScreens/NavigateScreens/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigateScreens
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void OnNextScreen(object sender, EventArgs e)
+        {
+            App.MessageToPass = "Hello from Main Screen!";
+
+            await Navigation.PushAsync(new SecondPage()); 
+
+        }
+    }
+}
